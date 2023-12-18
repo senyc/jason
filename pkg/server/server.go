99f160fa063acbfdc4075fdfe18b3388713304eb
senyc <95313103+senyc@users.noreply.github.com>
2023-12-17 21:15:05 -0500

    refactor: move handlers to separate file

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,78 +1,15 @@
 package server
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/senyc/jason/pkg/db"
-	"github.com/senyc/jason/pkg/types"
 )
 
 type Server struct {
 	db     *db.DB
 	server *http.Server
-}
-
-func (s *Server) getAllTasks(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	tasks, err := s.db.GetAllTasksByUser(vars["userId"])
-	if err != nil {
-		panic(err)
-	}
-	j, err := json.Marshal(tasks)
-
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write(j)
-
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (s *Server) addNewTask(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	var newTask types.NewTask
-
-	err := json.NewDecoder(req.Body).Decode(&newTask)
-	if err != nil {
-		panic(err)
-	}
-
-	err = s.db.AddNewTask(newTask, vars["userId"])
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		panic(err)
-	}
-	w.WriteHeader(http.StatusOK)
-}
-
-func (s *Server) getTaskById(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	task, err := s.db.GetTaskById(vars["userId"], vars["id"])
-	if err != nil {
-		panic(err)
-	}
-	j, err := json.Marshal(task)
-
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write(j)
-
-	if err != nil {
-		panic(err)
-	}
 }
 
 func (s *Server) Start() error {
