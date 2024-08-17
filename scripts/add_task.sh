60d1ca21ce3cdbe2886e568f03fbd27a101ef234
kyler <95313103+senyc@users.noreply.github.com>
2024-08-17 12:52:36 -0400

    feat: remove scripts

--- scripts/add_task.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-curl -H "Content-Type application/json" -H "Authorization Bearer eyJhbGciOiJFUzINiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiMmViYTIxNTUtMjkxNy0xMWVmLThiZDctMDI0MmFjMTQwMDAzIn0.MiW1JGCAk2-CJ7HeTO8wUqAbuPkzww-LOAwu0M8JE94zpS0PF0V_Ct52O2-l_ce693vGkm7gHCQwzXc1TW3LQg" -X POST localhost:8080/site/tasks/new -d '{"title": "this is a new task yay", "priority": 1}'
-
--- scripts/add_user.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -i -X POST localhost:8080/api/user/new -d '{"firstName": "fred", "lastName": "Sinatra", "email": "newtest@gmail.com", "password": "thisismypassword"}'
